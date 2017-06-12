7bb214da2b58a150746bf10c1961712cc2b7e4a2
Marc Laval <mlaval@amadeus.com>
2017-06-12 17:56:37 +0200

    First POC

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pwmetrics-ci
-WIP
+
+Result of POC:
+```
+{ url: 'http://localhost:5000/apps/ng-bootstrap-closure-compiler/',
+  timings: 
+   { ttfcp: 
+      { title: 'First Contentful Paint',
+        timing: '2580.31',
+        score: '2.24' },
+     ttfmp: 
+      { title: 'First Meaningful Paint',
+        timing: '2580.33',
+        score: '1.33' },
+     psi: 
+      { title: 'Perceptual Speed Index',
+        timing: '2625.53',
+        score: '1.38' },
+     fv: 
+      { title: 'First Visual Change',
+        timing: '2605.51',
+        score: '2.24' },
+     vc85: 
+      { title: 'Visually Complete 85%',
+        timing: '2606.17',
+        score: '1.32' },
+     vc100: 
+      { title: 'Visually Complete 100%',
+        timing: '2933.51',
+        score: '1.41' },
+     tti: 
+      { title: 'Time to Interactive (vAlpha)',
+        timing: '3456.17',
+        score: '1.75' } } }
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+const PWMetrics = require('pwmetrics');
+const serve = require('serve');
+global['atob'] = require('atob');
+
+const TIMES = 5;
+const TARGETS = ['http://localhost:5000/apps/aot-closure',
+  'http://localhost:5000/apps/ng-bootstrap-closure-compiler'
+];
+
+const tasks = [];
+TARGETS.forEach(target => {
+  tasks.push(() => getPerf(target));
+});
+
+const server = serve(__dirname, {
+  port: 5000,
+  ignore: ['node_modules']
+})
+ 
+
+promiseSerial(tasks)
+.then(results => {
+    const reference = extractResult(results[0]);
+    const target = extractResult(results[1]);
+    Object.keys(target.timings).forEach((id) => {
+      target.timings[id].score = (target.timings[id].timing / reference.timings[id].timing).toFixed(2);
+      target.timings[id].timing = (target.timings[id].timing).toFixed(2);
+    });
+    console.log(target);
+    server.stop();
+  })
+  .catch(console.error);
+
+function promiseSerial(promises) {
+  return promises.reduce((promise, func) =>
+      promise.then(result => func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]));
+}
+
+function getPerf(url) {
+  const pwMetrics = new PWMetrics(url, {
+    flags: {
+        runs: TIMES,
+        output: true
+    }
+  });
+  return pwMetrics.start();
+}
+
+function extractResult(result) {
+  const rawData = result.median ? result.median : result.runs[0];
+  const data = {
+    url: rawData.url,
+    timings: {}
+  };
+  rawData.timings.forEach((timing) => {
+    data.timings[timing.id] = {
+      title: timing.title,
+      timing: timing.timing
+    };
+  });
+  return data;
+}
